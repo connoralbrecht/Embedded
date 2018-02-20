fa798416cc1d348036fce53e7cf7aebbfff4e9d7
connoralbrecht <35579515+connoralbrecht@users.noreply.github.com>
2018-02-20 13:17:39 -0500

    Delete pushbutton.c

--- pushbutton.c
+++
@@ -1,9 +0,0 @@
-#include "pushbutton.h"
-
-void InitializePushButtonPortPin (void)
-{
-    // Configure port pin for pushbutton
-    ENABLE_PULL_UP_PULL_DOWN_RESISTORS;
-    SELECT_PULL_UP_RESISTORS;
-    SET_PUSHBUTTON_TO_AN_INPUT;
-}
